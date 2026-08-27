f45ee311dcddf6948ef8ee284fed216b0bebd1f4
apgiles116-boop <apgiles116@gmail.com>
2026-08-27 02:17:59 -0400

    Parameterize 5D SDP degree

--- kissing5d/run_threepoint.jl
+++ kissing5d/run_threepoint.jl
@@ -13,16 +13,16 @@ using .ThreePointBound
 
 n = 5
 costheta = 1//2
-for d in [4]
-    println("BEGIN n=$n costheta=$costheta d=$d")
-    problem, dualsol, primalsol = three_point_spherical_codes(
-        n, costheta, d, d;
-        prec=256,
-        duality_gap_threshold=1e-18,
-        omega_d=10^3,
-        omega_p=10^3,
-    )
-    println("PRIMAL d=$d value=", objvalue(problem, primalsol))
-    println("DUAL d=$d value=", dualobjvalue(problem, dualsol))
-    println("END d=$d")
-end
+d = length(ARGS) >= 1 ? parse(Int, ARGS[1]) : 4
+
+println("BEGIN n=$n costheta=$costheta d=$d")
+problem, dualsol, primalsol = three_point_spherical_codes(
+    n, costheta, d, d;
+    prec=256,
+    duality_gap_threshold=1e-18,
+    omega_d=10^3,
+    omega_p=10^3,
+)
+println("PRIMAL d=$d value=", objvalue(problem, primalsol))
+println("DUAL d=$d value=", dualobjvalue(problem, dualsol))
+println("END d=$d")
